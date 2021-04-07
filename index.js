c5be0725aab5a9f1249c52e56f66ba25f84fb394
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-04-07 21:25:31 +0300

    declare dependencies

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const util = require('./utils/generateMarkdown')
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
